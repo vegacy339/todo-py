7a05d5ed53f3e1fbe8a05eb35422324406ad7402
vegacy339 <vegacy339@gmail.com>
2025-12-04 20:24:16 +0400

    Add comments to the code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 
 todo_list = []
 
+# Menu display function
 def print_menu():
     print('Hi! You\'ve made it to the console todo list!')
     print('To view your to-do list, type !todo or /todo')
@@ -9,16 +10,19 @@ def print_menu():
     print('If you want to delete a task, type !delete or /delete')
     print('If you want to exit the program, type !quit or /quit')
 
+# Task list display function
 def show_tasks():
             if len(todo_list) == 0:
                 print('No tasks.')
             else:
                 for i, task in enumerate(todo_list, start=1):
                     print(f'{i}. {task}')
+# Add task function
 def add_task():
         user_add_task = input('Enter a task: ')
         todo_list.append(user_add_task)
         print(f'Task "{user_add_task}" added!')
+# Delete task function
 def delete_task():
         try:
              user_delete_task = int(input('Enter task number: '))
@@ -29,10 +33,12 @@ def delete_task():
                    print('Invalid task number!')
         except: 
               print('Error: Please enter a valid number!')
+# Program exit function
 def quit_func():
         print('Goodbye, good luck!')
         sys.exit()
 
+# Handler for user-entered commands
 def handle_command(command):
         if command == '!todo' or command == '/todo':
             show_tasks()
@@ -45,8 +51,10 @@ def handle_command(command):
         else:
             print('You entered an invalid command!')
 
+# Showing the menu
 print_menu()
 
+# The main program loop for processing user commands
 while True:
     command = input('Enter the command: ')
     handle_command(command)
